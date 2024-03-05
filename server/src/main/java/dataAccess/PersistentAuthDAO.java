ebdacbef61bfca8e430604da5850cf79d6dcb58d
CopperSands <masonjones227@gmail.com>
2024-03-05 14:48:12 -0700

    created PersistentAuthDAO constructor to create database and table if needed. Also created getAuth

--- server/src/main/java/dataAccess/PersistentAuthDAO.java
+++ server/src/main/java/dataAccess/PersistentAuthDAO.java
@@ -2,15 +2,61 @@ package dataAccess;
 
 import model.AuthData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PersistentAuthDAO implements AuthDAO{
+
+    private final String createTableSql = "CREATE TABLE IF NOT EXISTS auth(" +
+            "authToken CHAR(60) NOT NULL, " +
+            "userName VARCHAR NOT NULL," +
+            "PRIMARY KEY (authToken)" +
+            ")";
+    //check connection to database and creates new database if needed
+    public PersistentAuthDAO() throws DataAccessException{
+        try(Connection conn = DatabaseManager.getConnection()){
+            if(!isAuthTable(conn)){
+                createTable(conn);
+            }
+
+        }catch(DataAccessException e){
+            try{
+                //if the database doesn't exit
+                DatabaseManager.createDatabase();
+                try(Connection conn = DatabaseManager.getConnection()){
+                    createTable(conn);
+                } catch (SQLException ex) {
+                    throw new DataAccessException("Error internal server error");
+                }
+            } catch(DataAccessException ex){
+                throw ex;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error internal server error");
+        }
+    }
     @Override
     public void createAuth(String authToken, String username) throws DataAccessException {
-
+        //check if token exits
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        AuthData token = null;
+        try(Connection conn = DatabaseManager.getConnection()){
+            String sql = "SELECT * FROM auth WHERE authToken = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, authToken);
+            ResultSet result = stmt.executeQuery();
+            if (result.next()){
+                token = new AuthData(result.getString("authToken"), result.getString("userName"));
+            }
+            return token;
+        } catch (SQLException e) {
+            throw new DataAccessException("Error internal server error");
+        }
     }
 
     @Override
@@ -21,5 +67,26 @@ public class PersistentAuthDAO implements AuthDAO{
     @Override
     public void clear() throws DataAccessException {
 
+    }
+
+    private boolean isAuthTable(Connection conn) {
+        boolean isTable = true;
+        try {
+            PreparedStatement stmt = conn.prepareStatement("select count(*) from auth");
+            stmt.execute();
+            return isTable;
+        } catch (SQLException e) {
+            return false;
+        }
+    };
+
+
+    private void createTable(Connection conn) throws SQLException {
+        try{
+            PreparedStatement stmt = conn.prepareStatement(createTableSql);
+            stmt.executeUpdate();
+        }catch (SQLException e) {
+            throw e;
+        }
     }
 }
